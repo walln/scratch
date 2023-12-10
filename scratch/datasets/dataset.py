5749fcbab73f3eb2f47edba0120040e4a41cfd96
Nick <46641379+walln@users.noreply.github.com>
2023-12-10 17:33:43 -0600

    refactor: equniox testing (WIP)

--- scratch/datasets/dataset.py
+++ scratch/datasets/dataset.py
@@ -1,24 +1,34 @@
-from dataclasses import dataclass
+from dataclasses import dataclass  # noqa: D100, I001
 from typing import Generic, Optional, TypeVar
 
-from jax.numpy import ndarray
-from jax_dataloader import DataLoader as JaxDataLoader
-
+import jax
 from datasets import load_dataset
+from jax_dataloader import DataLoader as JaxDataLoader
 
 T = TypeVar("T")
 
 
 class Dataloader(Generic[T]):
+    """Wrapper around DataLoader that applies a transform to the batch."""
+
     def __init__(self, loader, transform):
+        """Initialize the dataloader.
+
+        Args:
+        ----
+            loader: the dataloader to wrap
+        transform: the transform to apply to the batch
+        """
         self.dataloader = loader
         self.transform = transform
         self.iterator = iter(self.dataloader)
 
     def __iter__(self):
+        """Return the iterator over the dataloader."""
         return self
 
     def __next__(self) -> T:
+        """Return the next batch."""
         try:
             batch = next(self.iterator)
             transformed_batch = self.transform(batch)
@@ -28,15 +38,25 @@ class Dataloader(Generic[T]):
             raise StopIteration
 
     def __len__(self):
+        """Return the number of batches in the dataloader."""
         return len(self.dataloader)
 
 
 class ImageClassificationBatch:
-    inputs: ndarray
-    targets: ndarray
+    """Batch of images and labels."""
+
+    inputs: jax.numpy.ndarray
+    targets: jax.numpy.ndarray
 
 
 def mnist_dataset(batch_size=32, shuffle=True):
+    """Load the MNIST dataset and return a Dataset object.
+
+    Args:
+    ----
+        batch_size: the batch size
+        shuffle: whether to shuffle the dataset
+    """
     dataset = load_dataset("mnist")
     train_loader = JaxDataLoader(
         dataset["train"], "jax", batch_size=batch_size, shuffle=shuffle
@@ -63,11 +83,38 @@ def mnist_dataset(batch_size=32, shuffle=True):
     )
 
 
+def cifar10_dataset(batch_size=32, shuffle=True):
+    """Load the CIFAR10 dataset and return a Dataset object.
+
+    Args:
+    ----
+        batch_size: the batch size
+        shuffle: whether to shuffle the dataset
+    """
+    dataset = load_dataset("cifar10")
+    train_loader = JaxDataLoader(
+        dataset["train"], "jax", batch_size=batch_size, shuffle=shuffle
+    )
+    test_loader = JaxDataLoader(
+        dataset["test"], "jax", batch_size=batch_size, shuffle=shuffle
+    )
+
+    def transform_colnames(batch):
+        # Scale pixel values from range [0, 255] to [0, 1]
+        batch["image"] = batch["image"].astype(float) / 255.0
+        return (batch["image"], batch["label"])
+
+    train_loader = Dataloader(train_loader, transform_colnames)
+    test_loader = Dataloader(test_loader, transform_colnames)
+
+    return Dataset[ImageClassificationBatch](
+        batch_size=batch_size, train=train_loader, test=test_loader, validation=None
+    )
+
+
 @dataclass
 class Dataset(Generic[T]):
-    """
-    Data module class that contains loaders
-    """
+    """Data module class that contains loaders."""
 
     batch_size: int
     train: Dataloader[T]
--- scratch/deep_learning/resnet/model.py
+++ scratch/deep_learning/resnet/model.py
@@ -1,136 +1,487 @@
-from functools import partial
-from typing import Any, Callable, Sequence, Tuple
+"""ResNet v1.5 model implementation. Adapted from torchvision.models.resnet."""
+from typing import Any, Callable, List, Optional, Sequence, Type, Union
 
+import equinox as eqx
+import jax
 import jax.numpy as jnp
-from flax import linen as nn
-
-ModuleDef = Any
+from jaxtyping import Array
 
 
-class ResNetBlock(nn.Module):
-    """ResNet block."""
+def _conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1, key=None):
+    """Construct a 3x3 convolution layer with padding.
 
-    filters: int
-    conv: ModuleDef
-    norm: ModuleDef
-    act: Callable
-    strides: Tuple[int, int] = (1, 1)
+    Args:
+    ----
+        in_planes: number of input channels
+        out_planes: number of output channels
+        stride: stride of convolution
+        groups: number of groups
+        dilation: dilation of convolution
+        key: random key for initialization
 
-    @nn.compact
-    def __call__(
+    Returns:
+    -------
+        Two-dimensional convolutional layer with 3x3 kernel size, padding, and
+        specified stride, groups, and dilation.
+    """
+    return eqx.nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        groups=groups,
+        use_bias=False,
+        dilation=dilation,
+        key=key,
+    )
+
+
+def _conv1x1(in_planes, out_planes, stride=1, key=None):
+    """Construct a 1x1 convolution layer.
+
+    Args:
+    ----
+        in_planes: number of input channels
+        out_planes: number of output channels
+        stride: stride of convolution
+        key: random key for initialization
+
+    Returns:
+    -------
+        Two-dimensional convolutional layer with 1x1 kernel size and specified
+        stride.
+    """
+    return eqx.nn.Conv2d(
+        in_planes, out_planes, kernel_size=1, stride=stride, use_bias=False, key=key
+    )
+
+
+class ResNetBlock(eqx.Module):
+    """Basic ResNet v1.5 block without bottleneck."""
+
+    expansion: int
+    conv_1: eqx.Module
+    batch_norm_1: eqx.Module
+    conv_2: eqx.Module
+    batch_norm_2: eqx.Module
+    relu: Callable[[jnp.ndarray], jnp.ndarray]
+    downsample: eqx.Module
+    stride: int
+
+    def __init__(
         self,
-        x,
+        inplanes,
+        planes,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+        norm_layer=None,
+        key=None,
     ):
-        residual = x
-        y = self.conv(self.filters, (3, 3), self.strides)(x)
-        y = self.norm()(y)
-        y = self.act(y)
-        y = self.conv(self.filters, (3, 3))(y)
-        y = self.norm(scale_init=nn.initializers.zeros_init())(y)
+        """Construct a basic ResNet block without bottleneck.
 
-        if residual.shape != y.shape:
-            residual = self.conv(self.filters, (1, 1), self.strides, name="conv_proj")(
-                residual
+        Args:
+        ----
+            inplanes: number of input channels
+            planes: number of output channels
+            stride: stride of convolution
+            downsample: downsampling layer
+            groups: number of groups
+            base_width: base width
+            dilation: dilation of convolution
+            norm_layer: normalization layer
+            key: random key for initialization
+
+        Raises:
+        ------
+            ValueError: if groups != 1 or base_width != 64
+            NotImplementedError: if dilation > 1
+        """
+        super(ResNetBlock, self).__init__()
+
+        if norm_layer is None:
+            norm_layer = eqx.nn.BatchNorm
+        if groups != 1 or base_width != 64:
+            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+
+        # self.conv1 and self.downsample layers downsample the input when stride != 1
+        keys = jax.random.split(key, 2)
+        self.expansion = 1
+        self.conv_1 = _conv3x3(inplanes, planes, stride, key=keys[0])
+        self.batch_norm_1 = norm_layer(planes, axis_name="batch")
+        self.conv_2 = _conv3x3(planes, planes, key=keys[1])
+        self.batch_norm_2 = norm_layer(planes, axis_name="batch")
+        self.relu = jax.nn.relu
+
+        if downsample:
+            self.downsample = downsample
+        else:
+            self.downsample = eqx.nn.Identity()
+
+        self.stride = stride
+
+    def __call__(self, x: Array, *, key: Optional[jax.random.PRNGKey] = None):
+        """Forward pass through the block."""
+        out = self.conv_1(x)
+        out = self.batch_norm_1(out)
+        out = self.relu(out)
+
+        out = self.conv_2(out)
+        out = self.batch_norm_2(out)
+
+        identity = self.downsample(x)
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNetBottleneckBlock(eqx.Module):
+    """Basic ResNet v1.5 block with bottleneck."""
+
+    expansion: int
+    conv_1: eqx.Module
+    batch_norm_1: eqx.Module
+    conv_2: eqx.Module
+    batch_norm_2: eqx.Module
+    conv_3: eqx.Module
+    batch_norm_3: eqx.Module
+    relu: Callable[[jnp.ndarray], jnp.ndarray]
+    downsample: eqx.Module
+    stride: int
+
+    def __init__(
+        self,
+        inplanes,
+        planes,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+        norm_layer=None,
+        key=None,
+    ):
+        """Construct a basic ResNet block with bottleneck.
+
+        Args:
+        ----
+            inplanes: number of input channels
+            planes: number of output channels
+            stride: stride of convolution
+            downsample: downsampling layer
+            groups: number of groups
+            base_width: base width
+            dilation: dilation of convolution
+            norm_layer: normalization layer
+            key: random key for initialization
+        """
+        super(ResNetBottleneckBlock, self).__init__()
+
+        if norm_layer is None:
+            norm_layer = eqx.nn.BatchNorm
+
+        self.expansion = 4
+        width = int(planes * (base_width / 64.0)) * groups
+
+        keys = jax.random.split(key, 3)
+
+        # conv_1 and downsample layers downsample the input when stride != 1
+        self.conv_1 = _conv1x1(inplanes, width, key=keys[0])
+        self.batch_norm_1 = norm_layer(width, axis_name="batch")
+        self.conv_2 = _conv3x3(width, width, stride, groups, dilation, key=keys[1])
+        self.batch_norm_2 = norm_layer(width, axis_name="batch")
+        self.conv_3 = _conv1x1(width, planes * self.expansion, key=keys[2])
+        self.batch_norm_3 = norm_layer(planes * self.expansion, axis_name="batch")
+        self.relu = jax.nn.relu
+
+        if downsample:
+            self.downsample = downsample
+        else:
+            self.downsample = eqx.nn.Identity()
+
+        self.stride = stride
+
+    def __call__(self, x: Array, *, key: Optional[jax.random.PRNGKey] = None):
+        """Forward pass through the block."""
+        out = self.conv_1(x)
+        out = self.batch_norm_1(out)
+        out = self.relu(out)
+
+        out = self.conv_2(out)
+        out = self.batch_norm_2(out)
+        out = self.relu(out)
+
+        out = self.conv_3(out)
+        out = self.batch_norm_3(out)
+
+        identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+EXPANSIONS = {ResNetBlock: 1, ResNetBottleneckBlock: 4}
+BlockType = Type[Union[ResNetBlock, ResNetBottleneckBlock]]
+
+
+class ResNet(eqx.Module):
+    """ResNet v1.5 model.
+
+    Adapted from torchvision.models.resnet.
+    """
+
+    inplanes: int
+    dilation: int
+    groups: Sequence[int]
+    base_width: int
+
+    conv_1: eqx.Module
+    batch_norm_1: eqx.Module
+    relu: jax.nn.relu
+    maxpool: eqx.Module
+    layer_1: eqx.Module
+    layer_2: eqx.Module
+    layer_3: eqx.Module
+    layer_4: eqx.Module
+    avgpool: eqx.Module
+    fc: eqx.Module
+
+    def __init__(
+        self,
+        block_type: Type[BlockType],
+        layers: List[int],
+        num_classes=10,
+        groups: int = 1,
+        width_per_group: int = 64,
+        replace_stride_with_dilation: List[bool] = None,
+        norm_layer: Any = None,
+        *,
+        key: Optional[jax.random.PRNGKey] = None,
+    ):
+        """Construct a ResNet v1.5 model.
+
+        Args:
+        ----
+            block_type: type of block to use
+            layers: number of blocks in each layer
+            num_classes: number of classes
+            groups: number of groups
+            width_per_group: width per group
+            replace_stride_with_dilation: replace stride with dilation
+            norm_layer: normalization layer
+            key: random key for initialization
+
+        """
+        super(ResNet, self).__init__()
+
+        if not norm_layer:
+            norm_layer = eqx.nn.BatchNorm
+
+        if norm_layer != eqx.nn.BatchNorm:
+            raise NotImplementedError("Unsupported norm_layer")
+
+        if key is None:
+            key = jax.random.PRNGKey(0)
+
+        keys = jax.random.split(key, 6)
+
+        self.inplanes = 64
+        self.dilation = 1
+        self.groups = groups
+        self.base_width = width_per_group
+
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                "or a 3-element tuple, got {}".format(replace_stride_with_dilation)
             )
-            residual = self.norm(name="norm_proj")(residual)
 
-        return self.act(residual + y)
-
-
-class BottleneckResNetBlock(nn.Module):
-    """Bottleneck ResNet block."""
-
-    filters: int
-    conv: ModuleDef
-    norm: ModuleDef
-    act: Callable
-    strides: Tuple[int, int] = (1, 1)
-
-    @nn.compact
-    def __call__(self, x):
-        residual = x
-        y = self.conv(self.filters, (1, 1))(x)
-        y = self.norm()(y)
-        y = self.act(y)
-        y = self.conv(self.filters, (3, 3), self.strides)(y)
-        y = self.norm()(y)
-        y = self.act(y)
-        y = self.conv(self.filters * 4, (1, 1))(y)
-        y = self.norm(scale_init=nn.initializers.zeros_init())(y)
-
-        if residual.shape != y.shape:
-            residual = self.conv(
-                self.filters * 4, (1, 1), self.strides, name="conv_proj"
-            )(residual)
-            residual = self.norm(name="norm_proj")(residual)
-
-        return self.act(residual + y)
-
-
-class ResNet(nn.Module):
-    """ResNetV1.5."""
-
-    stage_sizes: Sequence[int]
-    block_cls: ModuleDef
-    num_classes: int
-    num_filters: int = 64
-    dtype: Any = jnp.float32
-    act: Callable = nn.relu
-    conv: ModuleDef = nn.Conv
-
-    @nn.compact
-    def __call__(self, x, train: bool = True):
-        conv = partial(self.conv, use_bias=False, dtype=self.dtype)
-        norm = partial(
-            nn.BatchNorm,
-            use_running_average=not train,
-            momentum=0.9,
-            epsilon=1e-5,
-            dtype=self.dtype,
-            axis_name="batch",
+        self.conv_1 = eqx.nn.Conv2d(
+            3,
+            self.inplanes,
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            use_bias=False,
+            key=keys[0],
         )
 
-        x = conv(
-            self.num_filters,
-            (7, 7),
-            (2, 2),
-            padding=[(3, 3), (3, 3)],
-            name="conv_init",
-        )(x)
-        x = norm(name="bn_init")(x)
-        x = nn.relu(x)
-        x = nn.max_pool(x, (3, 3), strides=(2, 2), padding="SAME")
-        for i, block_size in enumerate(self.stage_sizes):
-            for j in range(block_size):
-                strides = (2, 2) if i > 0 and j == 0 else (1, 1)
-                x = self.block_cls(
-                    self.num_filters * 2**i,
-                    strides=strides,
-                    conv=conv,
-                    norm=norm,
-                    act=self.act,
-                )(x)
-        x = jnp.mean(x, axis=(1, 2))
-        x = nn.Dense(self.num_classes, dtype=self.dtype)(x)
-        x = jnp.asarray(x, self.dtype)
+        self.batch_norm_1 = norm_layer(input_size=self.inplanes, axis_name="batch")
+
+        self.relu = jax.nn.relu
+        self.maxpool = eqx.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer_1 = self._make_layer(
+            block_type, 64, layers[0], norm_layer, key=keys[1]
+        )
+        self.layer_2 = self._make_layer(
+            block_type,
+            128,
+            layers[1],
+            norm_layer,
+            stride=2,
+            dilate=replace_stride_with_dilation[0],
+            key=keys[2],
+        )
+        self.layer_3 = self._make_layer(
+            block_type,
+            256,
+            layers[2],
+            norm_layer,
+            stride=2,
+            dilate=replace_stride_with_dilation[1],
+            key=keys[3],
+        )
+        self.layer_4 = self._make_layer(
+            block_type,
+            512,
+            layers[3],
+            norm_layer,
+            stride=2,
+            dilate=replace_stride_with_dilation[2],
+            key=keys[4],
+        )
+
+        self.avgpool = eqx.nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = eqx.nn.Linear(512 * EXPANSIONS[block_type], num_classes, key=keys[5])
+
+    def _make_layer(
+        self,
+        block_type: BlockType,
+        planes: int,
+        blocks: int,
+        norm_layer: Any,
+        stride=1,
+        dilate=False,
+        key=None,
+    ):
+        """Construct a ResNet layer.
+
+        Args:
+        ----
+            block_type: block type to use
+            planes: number of output channels
+            blocks: number of blocks in the layer
+            norm_layer: normalization layer
+            stride: stride of convolution
+            dilate: whether to dilate convolution
+            key: random key for initialization
+
+        Returns:
+        -------
+            ResNet layer
+        """
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+
+        keys = jax.random.split(key, blocks + 1)
+        downsample = None
+
+        if stride != 1 or self.inplanes != planes * EXPANSIONS[block_type]:
+            downsample = eqx.nn.Sequential(
+                [
+                    _conv1x1(
+                        self.inplanes,
+                        planes * EXPANSIONS[block_type],
+                        stride=stride,
+                        key=keys[0],
+                    ),
+                    norm_layer(
+                        input_size=planes * EXPANSIONS[block_type], axis_name="batch"
+                    ),
+                ]
+            )
+
+        prev_dilation = self.dilation
+        layers = [
+            block_type(
+                self.inplanes,
+                planes,
+                stride,
+                downsample,
+                self.groups,
+                self.base_width,
+                prev_dilation,
+                norm_layer,
+                key=keys[1],
+            )
+        ]
+
+        self.inplanes = planes * EXPANSIONS[block_type]
+        for block_idx in range(1, blocks):
+            layers.append(
+                block_type(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                    key=keys[block_idx + 1],
+                )
+            )
+
+        return eqx.nn.Sequential(layers)
+
+    def __call__(self, x: Array, *, key: jax.random.PRNGKey) -> Array:
+        """Forward pass through the model.
+
+        Args:
+        ----
+            x: input array (batchless with 3 channels)
+            key: random key for initialization
+
+        Returns:
+        -------
+            Output of the model.
+
+        Raises:
+        ------
+            ValueError: if key is not provided.
+        """
+        if key is None:
+            raise ValueError("key must be provided")
+
+        keys = jax.random.split(key, 6)
+
+        x = self.conv_1(x, key=keys[0])
+        x = self.batch_norm_1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer_1(x, key=keys[1])
+        x = self.layer_2(x, key=keys[2])
+        x = self.layer_3(x, key=keys[3])
+        x = self.layer_4(x, key=keys[4])
+
+        x = self.avgpool(x)
+        x = jnp.ravel(x)
+        x = self.fc(x, key=keys[5])
+
         return x
 
 
-ResNet18 = partial(ResNet, stage_sizes=[2, 2, 2, 2], block_cls=ResNetBlock)
-ResNet34 = partial(ResNet, stage_sizes=[3, 4, 6, 3], block_cls=ResNetBlock)
-ResNet50 = partial(ResNet, stage_sizes=[3, 4, 6, 3], block_cls=BottleneckResNetBlock)
-ResNet101 = partial(ResNet, stage_sizes=[3, 4, 23, 3], block_cls=BottleneckResNetBlock)
-ResNet152 = partial(ResNet, stage_sizes=[3, 8, 36, 3], block_cls=BottleneckResNetBlock)
-ResNet200 = partial(ResNet, stage_sizes=[3, 24, 36, 3], block_cls=BottleneckResNetBlock)
+def resnet18(**kwargs) -> ResNet:
+    """Create a ResNet-18 model.
 
+    (Deep Residual Learning for Image Recognition)[https://arxiv.org/pdf/1512.03385.pdf]
 
-ResNet18Local = partial(
-    ResNet, stage_sizes=[2, 2, 2, 2], block_cls=ResNetBlock, conv=nn.ConvLocal
-)
-
-
-# Used for testing only.
-_ResNet1 = partial(ResNet, stage_sizes=[1], block_cls=ResNetBlock)
-_ResNet1Local = partial(
-    ResNet, stage_sizes=[1], block_cls=ResNetBlock, conv=nn.ConvLocal
-)
+    Args:
+    ----
+        kwargs: keyword arguments for `ResNet`
+    """
+    model = ResNet(ResNetBlock, [2, 2, 2, 2], **kwargs)
+    return model
