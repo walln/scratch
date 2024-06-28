18c0db0032157538f7d5cfb73d67bbdcff1f4022
Nick Wall <46641379+walln@users.noreply.github.com>
2024-06-28 00:18:22 -0500

    fix: always torch format datasets for zero copy perf

--- tests/deep_learning/models/image_classification/test_vit.py
+++ tests/deep_learning/models/image_classification/test_vit.py
@@ -1,25 +1,27 @@
-"""Tests for the ResNet model."""
+"""Tests for the ViT model."""
 
 from flax import nnx
 from scratch.datasets.image_classification_dataset import (
     dummy_image_classification_dataset,
 )
-from scratch.image_classification.resnet import ResNet, ResNetConfig
+from scratch.image_classification.vision_transformer import (
+    VisionTransformer,
+    VisionTransformerConfig,
+)
 
 
 def test_forward_pass():
-    """Test the forward pass of the ResNet."""
+    """Test the forward pass of the model."""
     batch_size = 2
-    shape = (28, 28, 1)
+    shape = (224, 224, 3)
     dataset = dummy_image_classification_dataset(
         shape=shape, batch_size=batch_size, num_samples=16
     )
-    model_config = ResNetConfig.from_preset(
-        preset_name="resnet18",
+    model_config = VisionTransformerConfig(
         num_classes=dataset.metadata.num_classes,
-        input_channels=shape[-1],
+        input_shape=shape,
     )
-    dummy_model = ResNet(config=model_config, rngs=nnx.Rngs(0))
+    dummy_model = VisionTransformer(model_config=model_config, rngs=nnx.Rngs(0))
     batch = next(iter(dataset.train))
-    y = dummy_model(batch["image"])
+    y = dummy_model(batch["image"].numpy())
     assert y.shape == (batch_size, dataset.metadata.num_classes)
